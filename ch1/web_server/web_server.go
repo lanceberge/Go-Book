410c1d6ded014b8c85536e3317fdf5c3ce90353b
lanceberge <bergeron.lance6@gmail.com>
2024-09-22 19:41:54 -0400

    flatten ch1 dir

---
+++ ch1/web_server/web_server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// use handler to handle requests at /
+	http.HandleFunc("/", handler)
+
+	// start a server on port 8000
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
